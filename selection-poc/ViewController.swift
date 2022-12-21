0dc3aa8d976bae0132dc89c18c2895f7bcb368fe
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-21 14:56:17 -0700

    Initial Commit

---
+++ selection-poc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  selection-poc
+//
+//  Created by Samuel Haycraft on 12/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
