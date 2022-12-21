f984e9fef3682b32e9789afc17197419288d8147
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-21 15:23:02 -0700

    Adds base skeleton map view

--- selection-poc/AppDelegate.swift
+++ selection-poc/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import ArcGIS
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AGSArcGISRuntimeEnvironment.apiKey = "AAPKd11368d6043b48449ba076433fe99388yh45QIVrF0fZtWddTOBleOGz6b1vf2bsBYy8YwqB4RHtXXHtPaXiZRe56RAyNeVe"
+        
         return true
     }
 
@@ -22,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -29,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        
     }
 
 
