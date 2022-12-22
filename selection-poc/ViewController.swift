6effe841688cd8bfacf8637e089402e52e16535c
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-22 00:00:40 -0700

    feat: adds line graphics overlay

--- selection-poc/ViewController.swift
+++ selection-poc/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         didSet {
             mapView.map = self.setupMap()
             mapView.setViewpoint(AGSViewpoint(latitude:34.02700, longitude: -118.80500, scale: 72_000))
+            mapView.graphicsOverlays.addObjects(from: [mapLineGraphicsOverlay()])
         }
     }
     
@@ -24,10 +25,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func mapLineGraphicsOverlay() -> AGSGraphicsOverlay {
+        // Create a simple fill symbol.
+        let lineSymbol = AGSSimpleLineSymbol(style: .solid, color: .red, width: 3.2)
+        // Create a graphics overlay for the square polygons.
+        let lineGraphicsOverlay = AGSGraphicsOverlay()
+        // Create and assign a simple renderer to the graphics overlay.
+        lineGraphicsOverlay.renderer = AGSSimpleRenderer(symbol: lineSymbol)
+        
+        // Create a polyline graphic with `AGSPolyline` geometry.
+        let polylineBuilder = AGSPolylineBuilder(spatialReference: .wgs84())
+        polylineBuilder.addPointWith(x: -118.80500, y: 34.02700)
+        polylineBuilder.addPointWith(x: -118.80, y: 34.02)
+        
+        
+        let polylineGraphic = AGSGraphic(geometry: polylineBuilder.toGeometry(), symbol: nil)
+        // Add the graphic to the overlay.
+        lineGraphicsOverlay.graphics.add(polylineGraphic)
+        return lineGraphicsOverlay
+    }
+    
+    
     private func setupMap() -> AGSMap {
         let map = AGSMap(
             basemapStyle: .arcGISTopographic
         )
+        //        let map = AGSMap(spatialReference: AGSSpatialReference(wkid: 4326)!)
+        map.basemap = AGSBasemap(style: .arcGISHillshadeDark)
         
         let featureLayer: AGSFeatureLayer = {
             let featureServiceURL = URL(string: SERVICE_URL)!
